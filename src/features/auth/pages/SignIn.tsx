e44316cec5d7418b0f5f157fab42c239289e7fe7
Manuel <caballeromanuel203@gmail.com>
2025-06-06 00:39:38 +0800

    updated sign in form layout

--- src/features/auth/pages/SignIn.tsx
+++ src/features/auth/pages/SignIn.tsx
@@ -1,8 +1,18 @@
+import { Button } from "@/components/ui/button";
 import { SignInForm } from "../components/SignInForm";
+import { FuelIcon } from "lucide-react";
 
 function SignIn() {
   return (
-    <div className="h-full flex justify-center items-center">
+    <div className="h-full flex justify-center items-center relative">
+      <div className="absolute flex gap-4 items-center top-8 left-8">
+        <Button>
+          <FuelIcon />
+        </Button>
+
+        <h1 className="font-semibold">Tetra V Inc.</h1>
+      </div>
+
       <SignInForm className="w-[25rem]" />
     </div>
   );
