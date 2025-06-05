c0c23bd1d37e3a47104ab391a010a69d234491a7
Manuel <caballeromanuel203@gmail.com>
2025-06-05 23:51:38 +0800

    react query setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import "./globals.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
