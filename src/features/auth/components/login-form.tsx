25ecdb103803ebe096c9bb2b52de4aa159f35c85
Manuel <caballeromanuel203@gmail.com>
2025-06-21 18:05:49 +0800

    created kogin form with zod and shadcn form

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -2,32 +2,107 @@ import { Button } from "@/components/ui/button";
 import { CardDescription } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { EyeIcon } from "lucide-react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginData, loginSchema } from "../types";
+import { useState } from "react";
 
 function LoginForm() {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const form = useForm<LoginData>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      remember_me: false,
+    },
+  });
+
+  const onSubmit = (data: LoginData) => {
+    console.log(data);
+  };
+
   return (
-    <div className="w-[25rem] grid gap-4 p-4">
-      <div className="grid gap-2">
-        <Label>Username</Label>
-        <Input />
-      </div>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-[25rem] grid gap-4 p-4"
+      >
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>
+                Username<span className="text-red-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-      <div className="grid gap-2">
-        <Label>Password</Label>
-        <div className="grid relative place-items-center">
-          <Input />
-          <EyeIcon className="absolute right-2" size={18} />
-        </div>
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>
+                Password<span className="text-red-500">*</span>
+              </FormLabel>
+              <div className="relative">
+                <FormControl>
+                  <Input type={showPassword ? "text" : "password"} {...field} />
+                </FormControl>
+                <button
+                  type="button"
+                  className="absolute right-2 top-1/2 -translate-y-1/2"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <EyeOffIcon size={18} />
+                  ) : (
+                    <EyeIcon size={18} />
+                  )}
+                </button>
+              </div>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-        <div className="flex items-center gap-2">
-          <Checkbox />
-          <CardDescription>Remember me</CardDescription>
-        </div>
-      </div>
+        <FormField
+          control={form.control}
+          name="remember_me"
+          render={({ field }) => (
+            <FormItem className="flex flex-row items-start space-x-3 space-y-0">
+              <FormControl>
+                <Checkbox
+                  checked={field.value}
+                  onCheckedChange={field.onChange}
+                />
+              </FormControl>
+              <div className="space-y-1 leading-none">
+                <CardDescription>Remember me</CardDescription>
+              </div>
+            </FormItem>
+          )}
+        />
 
-      <Button className="mt-2">Login</Button>
-    </div>
+        <Button type="submit" className="mt-2">
+          Login
+        </Button>
+      </form>
+    </Form>
   );
 }
 
---
+++ src/features/auth/types.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  username: z.string({ message: "Username is required." }),
+  password: z.string({ message: "Password is required." }),
+  remember_me: z.boolean(),
+});
+
+export type LoginData = z.infer<typeof loginSchema>;
